027fa86541033c26a080e31266128e51a0d4c8c4
Marcos Anton <marcos.anton@alumnos.frm.utn.edu.ar>
2025-01-24 16:47:28 -0300

    Creacion de los microservicios

---
+++ ms-booking-service/src/main/java/com/tutorial/msbookingservice/MsBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.msbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBookingServiceApplication.class, args);
+    }
+
+}
